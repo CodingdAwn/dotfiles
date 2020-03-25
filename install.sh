b60a0831ac0cdb85fbbef83bd857a66863a07a00
CodingdAwn <gaoxifengfordawn@gmail.com>
2020-03-25 23:28:06 +0800

    修改install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-dot_file_dir="~/dotfiles"
+# set path
+dot_file_dir=~/dotfiles
+home_dir=~/
+tmux_dir=~/.tmux
+echo "dot file dir is $dot_file_dir"
+echo "home dir is $home_dir"
 
-[ ! -d "$dot_file_dir" ] && mkdir -p "$dot_file_dir"
+#install tmux
+[ ! -d $dot_file_dir ] &&
+  git clone https://github.com/gpakosz/.tmux.git $home_dir/
 
 # 创建链接
-ln -s -f $dot_file_dir/.tmux.conf.local ~/
+ln -s -f $tmux_dir/.tmux.conf $home_dir/
+# 复制local配置到home
+cp -f $tmux_dir/.tmux.conf.local $home_dir/
+# 使用自己的conf
+echo "source-file $dot_file_dir/tmux.conf.myself" >> $home_dir/.tmux.conf.local
+
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 创建链接
+ln -s -f $dot_file_dir/.tmux.conf.local ~/
