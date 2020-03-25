c9ede5275e611c8104d21866bfa7d14f90d9f961
CodingdAwn <gaoxifengfordawn@gmail.com>
2020-03-26 01:26:55 +0800

    修复tmux启动失败的问题

--- install.sh
+++ install.sh
@@ -3,13 +3,21 @@
 # set path
 dot_file_dir=~/dotfiles
 home_dir=~/
-tmux_dir=~/.tmux
+github_repo_dir=~/github_repo
+tmux_dir=$github_repo_dir/tmux
 echo "dot file dir is $dot_file_dir"
 echo "home dir is $home_dir"
+echo "github repository dir is $github_repo_dir"
+echo "tmux dir is $tmux_dir"
 
+# clean files
+rm -f "$home_dir/.tmux.conf"
+rm -f "$home_dir/.tmux.conf.local"
+
+# set tmux repository url
+tmux_github_url="https://github.com/gpakosz/.tmux.git"
 #install tmux
-[ ! -d $dot_file_dir ] &&
-  git clone https://github.com/gpakosz/.tmux.git $home_dir/
+[ ! -d "$github_repo_dir" ] && mkdir -p $github_repo_dir && git clone "$tmux_github_url" "$tmux_dir"
 
 # 创建链接
 ln -s -f $tmux_dir/.tmux.conf $home_dir/
