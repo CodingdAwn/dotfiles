72df322cb1efebc92e3fca5b65efd95eeafe3613
CodingdAwn <gaoxifengfordawn@gmail.com>
2020-12-06 22:24:33 +0800

    a script for update vim-init, zsh, wiki

---
+++ update.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# update my repositories
+zshPath=~/dotfiles
+wikiPath=~/vimwiki
+vimPath=~/.vim/vim-init
+
+##########################################
+# git status
+##########################################
+Status()
+{
+  cd $zshPath
+  echo "current dir is $zshPath"
+  git status
+  
+  cd $wikiPath
+  echo "current dir is $wikiPath"
+  git status
+  
+  cd $vimPath
+  echo "current dir is $vimPath"
+  git status
+}
+
+##########################################
+# git pull
+##########################################
+Pull()
+{
+  cd $zshPath
+  echo "current dir is $zshPath"
+  git pull
+  
+  cd $wikiPath
+  echo "current dir is $wikiPath"
+  git pull
+  
+  cd $vimPath
+  echo "current dir is $vimPath"
+  git pull
+}
+
+##########################################
+# help
+##########################################
+Help()
+{
+  echo "#############################"
+  echo "this script is to do some git operates"
+  echo 
+  echo "-h        print this"
+  echo "-s        go dirs and git status"
+  echo "-p        go dirs and git pull"
+  echo "-push     not necessary, maybe in future"
+  echo "-commit   not necessary, maybe in future"
+  echo 
+  echo "#############################"
+}
+
+##########################################
+# main
+##########################################
+if [[ -z "$1" ]]
+then
+  echo "need a param, please input a operate"
+  Help
+  exit
+fi
+
+while getopts "hsp" param; do
+  case "${param}" in
+    s)
+      Status
+      exit;;
+    p)
+      Pull
+      exit;;
+    h | *)
+      Help
+      exit;;
+  esac
+done
