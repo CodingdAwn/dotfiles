6141b86283046085ced0e2b0a8d41e222602123a
dawn_ <gaoxifengfordawn@gmail.com>
2023-01-05 16:52:56 +0800

    使用新的kickstart.lua配置neovim

---
+++ nvim/after/plugin/treesitter.lua
@@ -0,0 +1,5 @@
+-- treesitter
+require('nvim-treesitter.configs').setup {
+  -- Add languages to be installed here that you want installed for treesitter
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'c_sharp' },
+}
