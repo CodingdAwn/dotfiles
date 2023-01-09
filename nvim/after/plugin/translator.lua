c0fd8dfd56f512a7338cc9662a99ce786a6cd9dd
dawn_ <gaoxifengfordawn@gmail.com>
2023-01-09 15:01:35 +0800

    use nvim as a submodule

--- nvim/after/plugin/translator.lua
+++
@@ -1,4 +0,0 @@
--- translator keymap
--- Echo translation in the cmdline
-vim.keymap.set('n', '<leader>tw', '<cmd>:TranslateW<CR>')
-vim.keymap.set('v', '<leader>tw', '<cmd>:TranslateWV<CR>')
--- nvim/after/plugin/vimwiki.lua
+++
@@ -1,2 +0,0 @@
---autocmd FileType wiki,vimwiki nnoremap <buffer> <leader>wd :VimwikiToggleListItem<CR>
-vim.api.nvim_create_autocmd("FileType", { pattern = { "wiki", "vimwiki" }, command = [[nnoremap <buffer> <leader>wd :VimwikiToggleListItem<CR>]] })
