1477ed645e7fc4939b1a9512c6680667c1fb0bfb
dawn_ <gaoxifengfordawn@gmail.com>
2023-09-05 17:37:15 +0800

    alpha custom modify

--- nvchad/configs/alpha.lua
+++ nvchad/configs/alpha.lua
@@ -7,16 +7,33 @@ local dashboard = require("alpha.themes.dashboard")
 -- TODO 想做一个每日的名句放在首页
 
 dashboard.section.header.val = {
-    "   .########.....###....##......##.##....##    ",
-    "   .##.....##...##.##...##..##..##.###...##    ",
-    "   .##.....##..##...##..##..##..##.####..##    ",
-    "   .##.....##.##.....##.##..##..##.##.##.##    ",
-    "   .##.....##.#########.##..##..##.##..####    ",
-    "   .##.....##.##.....##.##..##..##.##...###    ",
-    "   .########..##.....##..###..###..##....##    ",
-    "",
-    "",
-    "                               hi, i'm dAwn_",
+	"   .########.....###....##......##.##....##    ",
+	"   .##.....##...##.##...##..##..##.###...##    ",
+	"   .##.....##..##...##..##..##..##.####..##    ",
+	"   .##.....##.##.....##.##..##..##.##.##.##    ",
+	"   .##.....##.#########.##..##..##.##..####    ",
+	"   .##.....##.##.....##.##..##..##.##...###    ",
+	"   .########..##.....##..###..###..##....##    ",
+	"",
+	"",
+	"                            hi, it's dAwn_ here",
 }
 
+-- 找到配置目录
+local function get_nvchad_custom_path()
+	return "~/.config/nvim/init.lua"
+end
+
+-- custom buttons config
+local buttons = {
+	dashboard.button("f", "󰈞" .. "  Find File", "<CMD>Telescope find_files<CR>"),
+	dashboard.button("n", "" .. "  New File", "<CMD>ene!<CR>"),
+	dashboard.button("r", "" .. "  Recent files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "󰊄" .. "  Find Text", "<CMD>Telescope live_grep<CR>"),
+	dashboard.button("c", "" .. "  Configuration", "<CMD>edit " .. get_nvchad_custom_path() .. " <CR>"),
+	dashboard.button("q", "󰅖" .. "  Quit", "<CMD>quit<CR>"),
+}
+
+dashboard.section.buttons.val = buttons
+dashboard.section.footer.val = require'alpha.fortune'()
 alpha.setup(dashboard.config)
--- nvchad/mappings.lua
+++ nvchad/mappings.lua
@@ -3,63 +3,78 @@ local M = {}
 
 -- disable some origin mappings
 M.disabled = {
-  n = {
-    ["<A-h>"] = "",
-    ["<A-v>"] = "",
-    ["<leader>h"] = "",
-    ["<leader>v"] = "",
-  },
+	n = {
+		["<A-h>"] = "",
+		["<A-v>"] = "",
+		["<leader>h"] = "",
+		["<leader>v"] = "",
+		["<leader>q"] = "",
+	},
 
-  t = {
-    ["<A-h>"] = "",
-    ["<A-v>"] = "",
-  }
+	t = {
+		["<A-h>"] = "",
+		["<A-v>"] = "",
+	},
 }
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-    -- toggle transparency
-    ["<leader>tp"] = {
-      function()
-        require("base46").toggle_transparency()
-      end,
-      "toggle transparency",
-      opts = { nowait = true },
-    },
-  },
+		-- toggle transparency
+		["<leader>tp"] = {
+			function()
+				require("base46").toggle_transparency()
+			end,
+			"toggle transparency",
+			opts = { nowait = true },
+		},
+
+		-- close window
+		["<leader>q"] = { "<cmd>q<cr>", "close window", opts = { nowait = true } },
+	},
+}
+
+M.lsp = {
+	n = {
+		["<leader>ll"] = {
+			function()
+				vim.diagnostic.setloclist()
+			end,
+			"Diagnostic setloclist",
+		},
+	},
 }
 
 -- more keybinds!
 M.nvimtree = {
-  n = {
-    ["g?"] = { "lua require('nvim-tree.api').tree.toggle_help", "nvimtree Help" },
-  },
+	n = {
+		["g?"] = { "lua require('nvim-tree.api').tree.toggle_help", "nvimtree Help" },
+	},
 }
 
 M.vimwiki = {
-  n = {
-    ["<leader>ww"] = { "<cmd>VimwikiIndex<cr>", "index for vimwiki" },
-  },
+	n = {
+		["<leader>ww"] = { "<cmd>VimwikiIndex<cr>", "index for vimwiki" },
+	},
 }
 
 M.alpha = {
-  n = {
-    ["<leader>;"] = { "<cmd>Alpha<cr>", "dash board", opts = { nowait = true } },
-  },
+	n = {
+		["<leader>;"] = { "<cmd>Alpha<cr>", "dash board", opts = { nowait = true } },
+	},
 }
 
 M.translator = {
-  n = {
-    ["<leader>tw"] = { "<cmd>TranslateW<cr>", "translate current word", opts = { nowait = true } },
-    ["<leader>tt"] = { "<cmd>Translate<cr>", "translate current word to status line", opts = { nowait = true } },
-  },
+	n = {
+		["<leader>tw"] = { "<cmd>TranslateW<cr>", "translate current word", opts = { nowait = true } },
+		["<leader>tt"] = { "<cmd>Translate<cr>", "translate current word to status line", opts = { nowait = true } },
+	},
 
-  v = {
-    ["<leader>tw"] = { "<cmd>TranslateW<cr>", "translate current word", opts = { nowait = true } },
-    ["<leader>tt"] = { "<cmd>Translate<cr>", "translate current word to status line", opts = { nowait = true } },
-  },
+	v = {
+		["<leader>tw"] = { "<cmd>TranslateW<cr>", "translate current word", opts = { nowait = true } },
+		["<leader>tt"] = { "<cmd>Translate<cr>", "translate current word to status line", opts = { nowait = true } },
+	},
 }
 
 return M
