8d4d009e1bbcc5c2b5a607dfb194005d4faea7a8
dAwn <gaoxifengfordawn@gmail.com>
2024-05-08 11:02:29 +0800

    nvchad config modify

--- nvchad/mappings.lua
+++ nvchad/mappings.lua
@@ -6,6 +6,7 @@ nomap("n", "<A-v>")
 nomap("n", "<leader>h")
 nomap("n", "<leader>q")
 nomap("n", "<leader>b")
+nomap("n", "<leader>fm")
 
 nomap("t", "<A-h>")
 nomap("t", "<A-v>")
@@ -19,51 +20,55 @@ map("i", "jk", "<ESC>")
 
 -- toggle transparency
 map("n", "<leader>tp", function()
-    require("base46").toggle_transparency()
+  require("base46").toggle_transparency()
 end, { desc = "toggle transparency" })
+
+map("n", "<leader>fm", function()
+  vim.lsp.buf.format { async = true }
+end, { desc = "format file" })
 
 -- close window
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "close window" })
 
 -- increase width for windows
 map("n", "<leader>wl", function()
-    local width = vim.fn.winwidth(0)
-    if (width <= 50) then
-        vim.api.nvim_win_set_width(0, width + 10)
-    else
-        vim.api.nvim_win_set_width(0, width + width * 0.1)
-    end
+  local width = vim.fn.winwidth(0)
+  if width <= 50 then
+    vim.api.nvim_win_set_width(0, width + 10)
+  else
+    vim.api.nvim_win_set_width(0, width + width * 0.1)
+  end
 end, { desc = "increase width" })
 
 -- decrease width for windows
 map("n", "<leader>wh", function()
-    local width = vim.fn.winwidth(0)
-    if (width <= 50) then
-        vim.api.nvim_win_set_width(0, width - 10)
-    else
-        vim.api.nvim_win_set_width(0, width - width * 0.1)
-    end
+  local width = vim.fn.winwidth(0)
+  if width <= 50 then
+    vim.api.nvim_win_set_width(0, width - 10)
+  else
+    vim.api.nvim_win_set_width(0, width - width * 0.1)
+  end
 end, { desc = "decrease width" })
 
 -- clang lsp
 map("n", "<leader>aa", function()
-    if (vim.bo.ft == "cpp" or vim.bo.ft == "c") then
-        vim.cmd("ClangdSwitchSourceHeader ")
-    end
+  if vim.bo.ft == "cpp" or vim.bo.ft == "c" then
+    vim.cmd "ClangdSwitchSourceHeader "
+  end
 end, { desc = "switch header source with clangd" })
 
 map("n", "<leader>ah", function()
-    if (vim.bo.ft == "cpp" or vim.bo.ft == "c") then
-        vim.cmd("split")
-        vim.cmd("ClangdSwitchSourceHeader ")
-    end
+  if vim.bo.ft == "cpp" or vim.bo.ft == "c" then
+    vim.cmd "split"
+    vim.cmd "ClangdSwitchSourceHeader "
+  end
 end, { desc = "switch header source with clangd" })
 
 map("n", "<leader>av", function()
-    if (vim.bo.ft == "cpp" or vim.bo.ft == "c") then
-        vim.cmd("vsplit")
-        vim.cmd("ClangdSwitchSourceHeader ")
-    end
+  if vim.bo.ft == "cpp" or vim.bo.ft == "c" then
+    vim.cmd "vsplit"
+    vim.cmd "ClangdSwitchSourceHeader "
+  end
 end, { desc = "switch header source with clangd" })
 
 -- alpha
@@ -78,15 +83,15 @@ map("v", "<leader>tt", "<cmd>Translate<cr>", { desc = "translate current word to
 
 -- tabufline
 map("n", "<leader>bl", function()
-    require("nvchad.tabufline").tabuflineNext()
+  require("nvchad.tabufline").tabuflineNext()
 end, { desc = "Goto next buffer" })
 
 map("n", "<leader>bh", function()
-    require("nvchad.tabufline").tabuflinePrev()
+  require("nvchad.tabufline").tabuflinePrev()
 end, { desc = "Goto prev buffer" })
 
 map("n", "<leader>bh", function()
-    require("nvchad.tabufline").closeOtherBufs()
+  require("nvchad.tabufline").closeOtherBufs()
 end, { desc = "Close other buffers" })
 
 map("n", "<leader>bn", "<cmd> enew <CR>", { desc = "Close other buffers" })
