cf12c689293fadabf0f6dc417e1b77ba9f4f4baa
CodingdAwn <gaoxifengfordawn@gmail.com>
2025-12-28 16:13:52 +0800

    fish config

---
+++ fish/custom/alias.fish
@@ -0,0 +1,15 @@
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+function ....  ; cd ../../.. ; end
+function ..... ; cd ../../../.. ; end
+
+# Utilities
+function grep     ; command grep --color=auto $argv ; end
+
+# mv, rm, cp
+abbr mv 'mv -v'
+abbr rm 'rm -v'
+abbr cp 'cp -v'
+
+abbr cc 'clear'
