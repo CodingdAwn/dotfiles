d39a70bd60e883b6e6d8af463a7d484a012c7b8c
CodingdAwn <gaoxifengfordawn@gmail.com>
2026-01-08 19:40:43 +0800

    change tab width to  2

--- nvchad/lua/options.lua
+++ nvchad/lua/options.lua
@@ -4,22 +4,21 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-
 vim.opt.relativenumber = true
 -- 禁用交换文件
 vim.opt.swapfile = false
 
 -- 设置缩进宽度
-vim.opt.sw = 4
+vim.opt.sw = 2
 
 -- 设置 TAB 宽度
-vim.opt.ts = 4
+vim.opt.ts = 2
 
 -- 使用空格代替tab
 vim.opt.expandtab = true
 
 -- 如果后面设置了 expandtab 那么展开 tab 为多少字符
-vim.opt.softtabstop = 4
+vim.opt.softtabstop = 2
 
 vim.cmd('syntax on')
 vim.cmd('filetype plugin on')
