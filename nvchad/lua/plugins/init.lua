3eb60780cdebc1c0e5cda19d027478f29d00e0bb
CodingdAwn <gaoxifengfordawn@gmail.com>
2026-01-22 11:13:39 +0800

    leetcode.nvim

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -89,6 +89,91 @@ return {
     end,
   },
 
+  {
+    "kawre/leetcode.nvim",
+    -- build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+      -- include a picker of your choice, see picker section for more details
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = "Leet",
+    opts = {
+      {
+        arg = "leetcode.nvim",
+        lang = "cpp",
+        cn = { -- leetcode.cn
+          enabled = false,
+          translator = false,
+          translate_problems = false,
+        },
+        storage = {
+          home = "~/data",
+          cache = "~/cache",
+        },
+
+        plugins = {
+          non_standalone = false,
+        },
+
+        logging = true,
+        injector = {},
+        cache = {
+          update_interval = 60 * 60 * 24 * 7,
+        },
+
+        editor = {
+          reset_previous_code = true,
+          fold_imports = true,
+        },
+
+        console = {
+          open_on_runcode = true,
+          dir = "row",
+          size = {
+            width = "90%",
+            height = "75%",
+          },
+
+          result = {
+            size = "60%",
+          },
+
+          testcase = {
+            virt_text = true,
+            size = "40%",
+          },
+        },
+
+        description = {
+          position = "left",
+          width = "40%",
+          show_stats = true,
+        },
+
+        picker = { provider = nil },
+        hooks = {
+          ["enter"] = {},
+          ["question_enter"] = {},
+          ["leave"] = {},
+        },
+
+        keys = {
+          toggle = { "q" },
+          confirm = { "<CR>" },
+
+          reset_testcases = "r",
+          use_testcase = "U",
+          focus_testcases = "H",
+          focus_result = "L",
+        },
+
+        theme = {},
+        image_support = false,
+      }
+    },
+  }
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
