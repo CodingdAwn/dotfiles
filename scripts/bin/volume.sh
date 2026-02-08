b8dbf9683eec1c19051378bb4e105a8c6e03b3ee
CodingdAwn <gaoxifengfordawn@gmail.com>
2026-02-08 21:42:29 +0800

    update some comments

--- scripts/bin/volume.sh
+++ scripts/bin/volume.sh
@@ -7,5 +7,9 @@ pactl set-sink-volume @DEFAULT_SINK@ "${1}""2%"
 
 # get default sink
 default_sink=$(pactl get-default-sink)
+
+# set sink volume
 vol=$(pactl get-sink-volume ${default_sink} | awk -F'[/%]' '{print $2}' | tr -d ' ')
+
+# notify to dunst the volume info
 notify-send -h "int:value:${vol}" "Volume: ${vol}"
