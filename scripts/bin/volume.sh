eb1f2add47741cf0abfd5d71738343358ad01a6f
CodingdAwn <gaoxifengfordawn@gmail.com>
2026-02-08 21:50:17 +0800

    udpate volume.sh

--- scripts/bin/volume.sh
+++ scripts/bin/volume.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 if [[ $1 != '+' && $1 != '-' ]]; then
-  exit
+  exit 1
 fi
 
 pactl set-sink-volume @DEFAULT_SINK@ "${1}""2%"
 
 # get default sink
-default_sink=$(pactl get-default-sink)
+default_sink=$(pactl get-default-sink 2>/dev/null)
+if [[ -z "$default_sink" ]]; then
+    notify-send -u critical "pactl error" "default sink not found"
+    echo -e "error：default sink not found " >&2
+    exit 1
+fi
 
 # set sink volume
 vol=$(pactl get-sink-volume ${default_sink} | awk -F'[/%]' '{print $2}' | tr -d ' ')
