0688cf24e86418da83c01e24a50bbfe42351ebe0
CodingdAwn <gaoxifengfordawn@gmail.com>
2026-02-22 14:28:43 +0800

    zsh alias and mv fzf-wallpaper.sh

--- ohmyzsh/custom/alias.zsh
+++ ohmyzsh/custom/alias.zsh
@@ -11,4 +11,8 @@ alias ll="eza --long --header"
 alias ra="ranger"
 alias trans="trans :zh-CN -d"
 alias eza="eza --long --header"
+
+# fzf related
 alias fzf="fzf --height 50% --layout reverse --border"
+alias fzfbat='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+alias fzfwal="fzf-wallpaper.sh"
